cf885d028a5348d9d9c502b4a6ee36bea33b6712
geek-bhuvnesh <geek.bhuvnesh@gmail.com>
2015-08-26 18:14:30 +0530

    added middleware dependency

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var koa = require('koa');
-var app = koa();
+var app = module.exports = koa();
+var mount = require("koa-mount");
+var bodyParser = require('koa-bodyparser');
+
+//They take the data from your http POST and parse it into a more usable state
+app.use(require('koa-cors')({
+   methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS', //'POST,PUT,GET'                               //'GET,HEAD,PUT,POST,DELETE,OPTIONS',
+   credentials: true                                            //credentials: true
+ }));
 
 app.use(function *(){
   this.body = 'Welcome to PPL';
